39cd40ea130ee04118f546bd03473b2b25c7b662
Md. Imrul Hasan <mohammadhasan.imrul@gmail.com>
2022-10-27 16:49:58 +0600

    privat eroute loading state set

--- src/routes/PrivateRoute/PrivateRoute.js
+++ src/routes/PrivateRoute/PrivateRoute.js
@@ -1,13 +1,20 @@
+import { Spinner } from 'flowbite-react';
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../AuthProvider/AuthProvider';
 
-const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-    
-    if(!user){
-        return <Navigate to='/login' state={{from: location}} replace></Navigate>
+
+    if (loading) {
+        return <div className=" flex justify-center items-center">
+            <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 dark:border-gray-300"></div>
+        </div>
+    };
+
+    if (!user) {
+        return <Navigate to='/login' state={{ from: location }} replace></Navigate>
     }
     return children;
 };
